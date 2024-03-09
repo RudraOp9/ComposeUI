e1c52098c8e17fa61ab9d54475ea67c40e8e1f62
qwemnb <rudrapratapsinhchauhan1@gmail.com>
2024-03-09 19:30:46 +0530

    task 3 : login screen

--- app/src/main/java/com/leo/hellocompose/lerning.kt
+++ app/src/main/java/com/leo/hellocompose/lerning.kt
@@ -1,19 +1,18 @@
 package com.leo.hellocompose
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,7 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.leo.hellocompose.ui.theme.HelloComposeTheme
 
@@ -37,25 +35,27 @@ class lerning : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("hello world")
+                    Greeting2("hello world", context = this)
                 }
             }
         }
     }
 
 
-    private fun script(text: String){
-        Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+    fun script(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 
     @Composable
-    fun Greeting2(text: String, modifier: Modifier = Modifier) {
+    fun Greeting2(text: String, modifier: Modifier = Modifier,context: Context) {
 
-        Row (horizontalArrangement = Arrangement.Center , modifier = modifier){
-            Image(imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background)
-                , contentDescription = "image"
-                , modifier = modifier.padding(top = 20.dp)
-                    .clickable(true,null,null, onClick = {
+        Row(horizontalArrangement = Arrangement.Center, modifier = modifier) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "image",
+                modifier = modifier
+                    .padding(top = 20.dp)
+                    .clickable(true, null, null, onClick = {
                         script("i didn't said to click !!")
                     })
             )
@@ -63,22 +63,27 @@ class lerning : ComponentActivity() {
         }
 
 
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             Text(
                 text = text,
                 modifier = modifier
             )
-            Button(onClick = {script("you will be logged in soooon") } , ) {
+            Button(
+                onClick = {
+                    script("you will be logged in soooon")
+                    startActivity(Intent(context,MainActivity::class.java))
+
+                },
+            ) {
                 Text(text = "login")
             }
         }
 
     }
 }
-
-
-
-
 
 
 // task 1 : showing hello world - already provided by android
